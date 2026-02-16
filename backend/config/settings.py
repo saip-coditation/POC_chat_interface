4ac0bc505a6a6dab03cd853d9b4868f0c94a1676
saip-coditation <sai.phapale@coditation.com>
2026-02-16 16:08:45 +0530

    Fix STATIC_URL to serve files from root

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -101,7 +101,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files
-STATIC_URL = 'static/'
+STATIC_URL = '/'  # Serve static files from root to match hardcoded paths in index.html
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR.parent / 'css',
